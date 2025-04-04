08906713438f19c90cc898f1a8ae77da87d3e8da
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2025-04-04 10:18:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,14 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 #include <string>
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
+std::string infx2pstfx(const std::string& inf) {
   // добавьте код
   return std::string("");
 }
 
-int eval(std::string pref) {
+int eval(const std::string& pref) {
   // добавьте код
   return 0;
 }
