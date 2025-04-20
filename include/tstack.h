432960246745a41db617d4beb923cdf1a5d6b9bc
varvaraves <varvaraveselova01082006@mail.ru>
2025-04-20 13:44:48 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,38 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[size];
+  int top_index;
+
+ public:
+  TStack() : top_index(-1) {}
+
+  void Push(const T& value) {
+    if (top_index < size - 1) {
+      data[++top_index] = value;
+    }
+  }
+
+  T Pop() {
+    if (!IsEmpty()) {
+      return data[top_index--];
+    }
+    return T();
+  }
+
+  T Top() const {
+    if (!IsEmpty()) {
+      return data[top_index];
+    }
+    return T();
+  }
+
+  bool IsEmpty() const {
+    return top_index == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
